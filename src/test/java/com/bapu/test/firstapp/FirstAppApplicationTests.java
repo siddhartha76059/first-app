77a97fbabca569bc7ca3df8d2d90a403789f7219
Siddhartha <rautasiddhartha@gmail.com>
2020-01-29 23:50:00 +0530

    first commit

---
+++ src/test/java/com/bapu/test/firstapp/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bapu.test.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
