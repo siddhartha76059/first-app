52127569686ffa1145a21f542e393c70ef43877e
Siddhartha <rautasiddhartha@gmail.com>
2020-01-30 00:14:48 +0530

    Random Number Add

--- src/main/java/com/bapu/test/firstapp/controller/NamePrint.java
+++ src/main/java/com/bapu/test/firstapp/controller/NamePrint.java
@@ -4,10 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class NamePrint {
     @GetMapping("/hello/{name}")
     public String name(@PathVariable String name){
-        return "Hello " + name;
+        Random rn = new Random();
+        int a = rn.nextInt(1000);
+        return "["+a+"] Hello " + name;
     }
 }
